516709a43b3e9459aace46897fed1d058c3594b3
yousrik20 <yousri.k.e13@gmail.com>
2024-08-18 16:53:38 +0200

    Complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -19,7 +19,7 @@ export default function DashSidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to={'/dashboard?tab=profile'}>
-                    <Sidebar.Item active={tab=== 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    <Sidebar.Item as='div' active={tab=== 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
                         Profile
                     </Sidebar.Item>
                 </Link>
